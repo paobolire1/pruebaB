2d501780f3178890401444ba0aceccf497d532ea
paobolire1 <78909180+paobolire1@users.noreply.github.com>
2021-02-11 06:36:27 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,8 @@
+var createScene = function() {
+    
+    var scene = new BABYLON.Scene(engine);
+    // Add a camera to the scene and attach it to the canvas
+    // Add a lights to the scene
+    //Your Code
+  return scene;
+};
